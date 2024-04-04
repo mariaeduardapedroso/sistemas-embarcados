3142cc9a9ac441e6b894afcbc5811880a0c0d329
Maria Eduarda Pedroso <mpedroso@alunos.utfpr.edu.br>
2024-04-04 14:32:23 -0300

    Update main.ino

--- atividade1/main.ino
+++ atividade1/main.ino
@@ -2,14 +2,6 @@
 #define _TIMERINTERRUPT_LOGLEVEL_ 0
 #define USE_TIMER_1 true
 
-#if (defined(__AVR_ATmega644__) || defined(__AVR_ATmega644A__) || defined(__AVR_ATmega644P__) || defined(__AVR_ATmega644PA__) || defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_NANO) || defined(ARDUINO_AVR_MINI) || defined(ARDUINO_AVR_ETHERNET) || defined(ARDUINO_AVR_FIO) || defined(ARDUINO_AVR_BT) || defined(ARDUINO_AVR_LILYPAD) || defined(ARDUINO_AVR_PRO) || defined(ARDUINO_AVR_NG) || defined(ARDUINO_AVR_UNO_WIFI_DEV_ED) || defined(ARDUINO_AVR_DUEMILANOVE) || defined(ARDUINO_AVR_FEATHER328P) || defined(ARDUINO_AVR_METRO) || defined(ARDUINO_AVR_PROTRINKET5) || defined(ARDUINO_AVR_PROTRINKET3) || defined(ARDUINO_AVR_PROTRINKET5FTDI) || defined(ARDUINO_AVR_PROTRINKET3FTDI))
-#define USE_TIMER_2 true
-#warning Using Timer1, Timer2
-#else
-#define USE_TIMER_3 true
-#warning Using Timer1, Timer3
-#endif
-
 // To be included only in main(), .ino with setup() to avoid `Multiple Definitions` Linker Error
 #include "TimerInterrupt.h"
 
